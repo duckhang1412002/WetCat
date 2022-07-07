28f46ba00a255b9c0e4adf94bc4740ee9bbff0e4
nghinqce160201 <106872476+nghinqce160201@users.noreply.github.com>
2022-07-07 21:52:45 +0700

    Delete Post at Admin page,Warning list Anotation, UserManagement fixed

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -18,6 +18,30 @@ namespace WetCat.Controllers
         public IActionResult Index(){
             var postLists = postList.GetAllPosts().ToList();
             return View(postLists);
+        }       
+
+        public ActionResult Delete(int? id){
+            if (id == null){
+                return NotFound();
+            }
+            var p = postList.FindID(id.Value);
+            if (p == null){
+                return NotFound();
+            }
+            return View(p);
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id){
+            try {
+                postList.Remove(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex){
+                ViewBag.Message = ex.Message;
+                return View();
+            }
+        }
+
     }
 }
